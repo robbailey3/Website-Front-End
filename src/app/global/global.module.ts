19d0380c06043be4bc7017e005eb0992c4a05b86
Rob <rob.bailey3@gmail.com>
2019-01-11 17:01:04 +0000

    More Updates

--- src/app/global/global.module.ts
+++ src/app/global/global.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation/navigation.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [NavigationComponent, FooterComponent],
   exports: [NavigationComponent, FooterComponent],
-  imports: [CommonModule, RouterModule]
+  imports: [CommonModule, RouterModule, SharedModule]
 })
 export class GlobalModule {}
