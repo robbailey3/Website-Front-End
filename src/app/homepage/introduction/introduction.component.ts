23385ba28eff64e5b6a1d4d6a302eff0e6e8d657
Rob <rob.bailey3@gmail.com>
2019-03-18 17:44:25 +0000

    New Feature: Adding in timeline to homepage

--- src/app/homepage/introduction/introduction.component.ts
+++ src/app/homepage/introduction/introduction.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./introduction.component.scss']
 })
 export class IntroductionComponent implements OnInit {
+  public now = new Date().getFullYear();
+  public history = [
+    {
+      title: 'Quality Control Branding Specialist',
+      date: 'January 2018 - Present',
+      description: 'lorem ipsum ...'
+    },
+    {
+      title: 'Quality Control Executive',
+      date: 'March 2016 - January 2018',
+      description: 'foo bar'
+    },
+    {
+      title: 'Keele University',
+      date: 'September 2012 - June 2015',
+      description: ''
+    }
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
